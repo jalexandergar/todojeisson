0ed5542535bdbc8b501e54f6240ed5173291192e
JEISSON ALEXANDER GARCIA AGUIRRE <jalexandergar>
2021-08-26 19:59:00 -0500

    Structure

---
+++ todojeisson.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todojeisson.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
