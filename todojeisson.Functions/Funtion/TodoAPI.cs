e0dd73ed706f8ece92a35366dd927522a9e4385f
JEISSON ALEXANDER GARCIA AGUIRRE <jalexandergar>
2021-09-02 20:18:39 -0500

    Se agrega el todoentity

--- todojeisson.Functions/Funtion/TodoAPI.cs
+++ todojeisson.Functions/Funtion/TodoAPI.cs
@@ -7,23 +7,42 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
+using todojeisson.Common.Models;
+using todojeisson.Common.Responses;
+using todojeisson.Functions.Entities;
 
 namespace todojeisson.Functions.Funtion
 {
     public static class TodoAPI
     {
-        [FunctionName("TodoAPI")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName(nameof(CreateTodo))]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req,
+            [Table("todo",Connection = "AzureWebJobsStorage")] ICloneable todoTable,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Recived a nre todo.");
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            Todo todo = JsonConvert.DeserializeObject<Todo>(requestBody);
+
+            if(string.IsNullOrEmpty(todo?.TaskDescription))
+            {
+                return new BadRequestObjectResult(new Response { IsSuccess = false, Message = "The request must have a Task description." });
+            }
+
+            TodoEntity todoEntity = new TodoEntity
+            {
+                CreateTime = DateTime.UtcNow,
+                ETag = "*",
+                IsCompleted = false,
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                TaskDescription = todo.TaskDescription
+            };
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
